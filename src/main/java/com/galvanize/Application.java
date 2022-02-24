d4e5a0c7e6a538e25b552deb677c46b65724b8d8
lanceredfearn <lance.c.redfearn.mil@swf.army.mil>
2022-02-24 11:16:59 -0600

    Changes to gradle build

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,7 +1,11 @@
 package com.galvanize;
+// I'm still not sure what a package is.
 
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public class Application {
+//100% sure I don't know what a class is exactly.
+        public static void main(String[] args) {
+//This is required for some reason!
+            System.out.print("Hello World. I'm just going to add some erroneous text in here to lengthen the amount of data. I would like to see what the difference is between the print statements. Maybe I need to use enough text to cover more than the screen size.");
+//This is the only portion I actually understand.
     }
 }
